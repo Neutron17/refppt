1d0953c448315ee82d6ef3bdf276019b27e3c6cb
neutron17 <neutron17771@gmail.com>
2025-07-06 16:15:16 +0200

    Error handling, logging with log4j

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 package com.neutron
 
+/*import org.crosswire.jsword.book.BookData
+import org.crosswire.jsword.book.Books
+import org.crosswire.jsword.book.OSISUtil
+import org.crosswire.jsword.bridge.BookInstaller
+import org.crosswire.jsword.passage.Verse*/
+import org.apache.logging.log4j.LogManager
 import org.apache.poi.sl.usermodel.PictureData
 import org.apache.poi.xslf.usermodel.XMLSlideShow
 import org.apache.poi.xslf.usermodel.XSLFPictureData
@@ -10,16 +16,21 @@ import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 import javax.imageio.ImageIO
-import kotlin.contracts.contract
+import kotlin.system.exitProcess
 
 //"https://enekeskonyv.reformatus.hu/digitalis-reformatus-enekeskonyv/enek/3/"
 
-val SLIDE_W = 960
-val SLIDE_H = 540
-val SIDE_PADDING = 50
+const val SLIDE_W = 960
+const val SLIDE_H = 540
 
-data class Song(val id:Int, val pd: BufferedImage, val verses:List<Int>)
+val log = LogManager.getLogger()!!
+
+sealed interface Data
+
+data class Song(val id: Int, val pd: BufferedImage, val verses: List<Int>):Data
+data class Passage(val path: String):Data
 
 fun bufimgToPicData(ppt: XMLSlideShow, bufimg: BufferedImage): XSLFPictureData {
     val baos = ByteArrayOutputStream()
@@ -28,22 +39,28 @@ fun bufimgToPicData(ppt: XMLSlideShow, bufimg: BufferedImage): XSLFPictureData {
     return ppt.addPicture(baos.toByteArray(), PictureData.PictureType.JPEG)
 }
 
-fun parse(str:String): ArrayList<Song> {
-    val ret = arrayListOf<Song>()
+fun parse(str:String): ArrayList<Data> {
+    val ret = arrayListOf<Data>()
     for(line in str.split("\n")) {
         if(line[0].isDigit()) { // song
             //val id = line.takeWhile { it.isDigit() }.toInt()
 
             val id = line.split("/")[0].toInt()
             val bufimg = ImageIO.read(File("images/$id.jpg"))
-            println("${bufimg.width}x${bufimg.height}\tq=${bufimg.height.toFloat()/bufimg.width.toFloat()}")
 
             val lyrfile = File("lyr/$id.txt")
-            val max = lyrfile.readLines().count { it.isBlank() }
+            val max = try {
+                lyrfile.readLines().count { it.isBlank() } // TODO: fix reopening
+            } catch(e: IOException) {
+                e.printStackTrace()
+                log.fatal("Couldn't read lyrics file with ID=$id. ${e.localizedMessage}. Aborting")
+                exitProcess(1)
+            }
 
             val tmp = try {
                     line.split("/")[1]
                 } catch (_: IndexOutOfBoundsException) {
+                    log.debug("'$line' is not slash separated")
                     val foo = line.split("-")
                     ret.add(Song(id, bufimg,(foo[0].toInt()..foo[1].toInt()).toList().dropWhile { it == 1 }))
                     continue
@@ -53,30 +70,66 @@ fun parse(str:String): ArrayList<Song> {
             } else {
                 tmp.split(",").map { it.toInt() }
             }
-            ret.add(Song(id, bufimg, verses))
+            ret.add(Song(id, bufimg, verses.dropWhile { it == 1 }))
+        } else { // passage
+            ret.add(Passage(line))
         }
     }
     return ret
 }
 
-fun main() {
+fun main(args: Array<String>) {
+    /*//BookInstaller.main(args)
+    val bible = Books.installed().getBook("HunRUF")
+    val keys = bible.getKey("2Jn 1:11-13")
+    val data = BookData(bible, keys)
+    println(OSISUtil.getCanonicalText(data.osisFragment))
+    println()
+    for(key in keys) {
+        val verse = key.to(Verse())
+        println(bible.getRawText(verse))
+    }
+    return*/
+    log.info("Creating presentation with dimensions ${SLIDE_W}x$SLIDE_H")
     val ppt = XMLSlideShow()
     ppt.pageSize = Dimension(SLIDE_W, SLIDE_H)
 
+    log.info("Creating first blank slide")
     val slide1 = ppt.createSlide()
     slide1.background.fillColor = Color.BLACK
 
-    parse(File("instr.txt").readText()).forEach { (id:Int, image:BufferedImage, verses) ->
-        imageSlide(ppt, image)
-        verseSlide(ppt, id, verses)
+    try {
+        parse(File("instr.txt").readText()).forEach { it ->
+            when (it) {
+                is Song -> {
+                    log.debug("Creating slide(s) for ID=${it.id}")
+                    imageSlide(ppt, it.pd)
+                    verseSlide(ppt, it.id, it.verses)
+                }
+
+                is Passage -> {
+                    scriptureSlide(ppt, it.path)
+                }
+            }
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+        log.fatal("Couldn't read file 'instr.txt'. ${e.localizedMessage}. Aborting")
+        exitProcess(1)
     }
 
+    log.info("Creating last blank slide")
     val endslide = ppt.createSlide()
     endslide.background.fillColor = Color.BLACK
+    log.info("Writing PPT to file")
     ppt.write(FileOutputStream("test.pptx"))
 }
 
 fun verseSlide(ppt: XMLSlideShow, songId:Int, verses: List<Int>) {
+    if(verses.isEmpty()) {
+        log.debug("empty verses while creating verse slide, skipping")
+        return
+    }
     val HPAD = 5
     val VPAD = 5
 
@@ -86,17 +139,24 @@ fun verseSlide(ppt: XMLSlideShow, songId:Int, verses: List<Int>) {
     var textBox = slide.createTextBox()
     textBox.anchor = Rectangle(HPAD, VPAD, SLIDE_W-2*HPAD, SLIDE_H-2*VPAD)
     for((index, verse) in verses.withIndex()) {
+        log.debug("index=$index verse=$verse")
         val paragraph = textBox.addNewTextParagraph()
         val run = paragraph.addNewTextRun()
         val versetexts = arrayListOf<String>()
         var curr = ""
-        File("lyr/$songId.txt").readLines().forEach {
-            if(it.isBlank()) {
-                versetexts.add(curr)
-                curr = ""
-                return@forEach
+        try {
+            File("lyr/$songId.txt").readLines().forEach {
+                if (it.isBlank()) {
+                    versetexts.add(curr)
+                    curr = ""
+                    return@forEach
+                }
+                curr += it + "\n"
             }
-            curr += it+"\n"
+        } catch (e: IOException) {
+            log.error("Failed to open lyrics file with id: $songId. ${e.localizedMessage}. Aborting")
+            e.printStackTrace()
+            exitProcess(2);
         }
         with(run) {
             setText("$verse. ${versetexts[verse-1]}")
@@ -105,6 +165,7 @@ fun verseSlide(ppt: XMLSlideShow, songId:Int, verses: List<Int>) {
         }
 
         if(index % 2 == 1 && index != verses.size - 1) {
+            log.debug("new verse slide, index=$index")
             slide = ppt.createSlide()
             slide.background.fillColor = Color.BLACK
             textBox = slide.createTextBox()
@@ -114,10 +175,20 @@ fun verseSlide(ppt: XMLSlideShow, songId:Int, verses: List<Int>) {
 }
 
 fun scriptureSlide(ppt: XMLSlideShow, path: String) {
+    val slide = ppt.createSlide()
+    slide.background.fillColor = Color.BLACK
 
+    slide.createTextBox().apply {
+        anchor = Rectangle(0, 0, SLIDE_W, SLIDE_H)
+    }.addNewTextParagraph().addNewTextRun().run {
+        setText(path)
+        setFontColor(Color.WHITE)
+        fontSize = 64.0
+    }
 }
 
 fun imageSlide(ppt: XMLSlideShow, image: BufferedImage) {
+    log.debug("Creating image slide ${image.width}x${image.height}, q=${image.height.toFloat() / image.width.toFloat()}")
     assert(image.width == 800)
     val pd = bufimgToPicData(ppt, image)
 
@@ -127,17 +198,13 @@ fun imageSlide(ppt: XMLSlideShow, image: BufferedImage) {
 
 
     if(image.height < 700) { // single slide
-        println("single")
+        log.debug("single slide")
         chordPic.anchor = Rectangle(
             (SLIDE_W - image.width * SLIDE_H / image.height) / 2, 0,
             image.width * SLIDE_H / image.height, SLIDE_H
         )
     } else { // multi slide
-        println("multi")
-        /*chordPic.anchor = Rectangle(
-            (SIDE_PADDING), 0,
-            SLIDE_W - 2*SIDE_PADDING, SLIDE_H
-        )*/
+        log.debug("multiple slides")
         chordPic.anchor = Rectangle((SLIDE_W - image.width) / 2, 0, image.width, image.height)
         val overflowSlide = ppt.createSlide()
         overflowSlide.background.fillColor = Color.BLACK
@@ -146,7 +213,8 @@ fun imageSlide(ppt: XMLSlideShow, image: BufferedImage) {
             (SLIDE_W - image.width) / 2, SLIDE_H-image.height,
             image.width, image.height
         )
-        if(image.height > SLIDE_H-10) {
+        if(image.height > 2*SLIDE_H-10) {
+            log.info("Image doesn't fit on 2 slides")
             arrayOf(slide, overflowSlide).forEach {
                 it.createTextBox().run {
                     text = "BAD"
