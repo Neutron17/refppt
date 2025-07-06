6c24a0574f7e4b0cf1ff4f98d885c4ad29109857
neutron17 <neutron17771@gmail.com>
2025-07-06 16:39:27 +0200

    refactor, new files

---
+++ src/main/kotlin/Data.kt
@@ -0,0 +1,47 @@
+package com.neutron
+
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.IOException
+import javax.imageio.ImageIO
+import kotlin.system.exitProcess
+
+sealed interface Data {
+    companion object {
+        fun fromString(line: String): Data {
+            if(line[0].isDigit()) { // song
+                val id = line.split("/")[0].toInt()
+                val bufimg = ImageIO.read(File("images/$id.jpg"))
+
+                val lyrfile = File("lyr/$id.txt")
+                val max = try {
+                    lyrfile.readLines().count { it.isBlank() } // TODO: fix reopening
+                } catch(e: IOException) {
+                    e.printStackTrace()
+                    log.fatal("Couldn't read lyrics file with ID=$id. ${e.localizedMessage}. Aborting")
+                    exitProcess(1)
+                }
+
+                val tmp = try {
+                    line.split("/")[1]
+                } catch (_: IndexOutOfBoundsException) {
+                    log.debug("'$line' is not slash separated")
+                    val foo = line.split("-")
+                    return Song(id, bufimg,(foo[0].toInt()..foo[1].toInt()).toList().dropWhile { it == 1 })
+                }
+                val verses: List<Int> = if(tmp[0] == 'a') {
+                    (1..max).toList()
+                } else {
+                    tmp.split(",").map { it.toInt() }
+                }
+                return Song(id, bufimg, verses.dropWhile { it == 1 })
+            } else { // passage
+                return Passage(line)
+            }
+        }
+    }
+}
+
+data class Song(val id: Int, val pd: BufferedImage, val verses: List<Int>):Data
+data class Passage(val path: String):Data
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,55 +27,11 @@ const val SLIDE_H = 540
 
 val log = LogManager.getLogger()!!
 
-sealed interface Data
-
-data class Song(val id: Int, val pd: BufferedImage, val verses: List<Int>):Data
-data class Passage(val path: String):Data
-
 fun bufimgToPicData(ppt: XMLSlideShow, bufimg: BufferedImage): XSLFPictureData {
     val baos = ByteArrayOutputStream()
     ImageIO.write(bufimg, "jpg", baos)
     baos.close()
     return ppt.addPicture(baos.toByteArray(), PictureData.PictureType.JPEG)
-}
-
-fun parse(str:String): ArrayList<Data> {
-    val ret = arrayListOf<Data>()
-    for(line in str.split("\n")) {
-        if(line[0].isDigit()) { // song
-            //val id = line.takeWhile { it.isDigit() }.toInt()
-
-            val id = line.split("/")[0].toInt()
-            val bufimg = ImageIO.read(File("images/$id.jpg"))
-
-            val lyrfile = File("lyr/$id.txt")
-            val max = try {
-                lyrfile.readLines().count { it.isBlank() } // TODO: fix reopening
-            } catch(e: IOException) {
-                e.printStackTrace()
-                log.fatal("Couldn't read lyrics file with ID=$id. ${e.localizedMessage}. Aborting")
-                exitProcess(1)
-            }
-
-            val tmp = try {
-                    line.split("/")[1]
-                } catch (_: IndexOutOfBoundsException) {
-                    log.debug("'$line' is not slash separated")
-                    val foo = line.split("-")
-                    ret.add(Song(id, bufimg,(foo[0].toInt()..foo[1].toInt()).toList().dropWhile { it == 1 }))
-                    continue
-                }
-            val verses: List<Int> = if(tmp[0] == 'a') {
-                (1..max).toList()
-            } else {
-                tmp.split(",").map { it.toInt() }
-            }
-            ret.add(Song(id, bufimg, verses.dropWhile { it == 1 }))
-        } else { // passage
-            ret.add(Passage(line))
-        }
-    }
-    return ret
 }
 
 fun main(args: Array<String>) {
@@ -99,16 +55,16 @@ fun main(args: Array<String>) {
     slide1.background.fillColor = Color.BLACK
 
     try {
-        parse(File("instr.txt").readText()).forEach { it ->
-            when (it) {
+        File("instr.txt").readText().split("\n").forEach {
+            val data = Data.fromString(it)
+            when (data) {
                 is Song -> {
-                    log.debug("Creating slide(s) for ID=${it.id}")
-                    imageSlide(ppt, it.pd)
-                    verseSlide(ppt, it.id, it.verses)
+                    log.debug("Creating slide(s) for ID=${data.id}")
+                    ppt.imageSlide(data.pd)
+                    ppt.verseSlide(data.id, data.verses)
                 }
-
                 is Passage -> {
-                    scriptureSlide(ppt, it.path)
+                    ppt.scriptureSlide(data.path)
                 }
             }
         }
@@ -123,105 +79,4 @@ fun main(args: Array<String>) {
     endslide.background.fillColor = Color.BLACK
     log.info("Writing PPT to file")
     ppt.write(FileOutputStream("test.pptx"))
-}
-
-fun verseSlide(ppt: XMLSlideShow, songId:Int, verses: List<Int>) {
-    if(verses.isEmpty()) {
-        log.debug("empty verses while creating verse slide, skipping")
-        return
-    }
-    val HPAD = 5
-    val VPAD = 5
-
-    var slide = ppt.createSlide()
-    slide.background.fillColor = Color.BLACK
-
-    var textBox = slide.createTextBox()
-    textBox.anchor = Rectangle(HPAD, VPAD, SLIDE_W-2*HPAD, SLIDE_H-2*VPAD)
-    for((index, verse) in verses.withIndex()) {
-        log.debug("index=$index verse=$verse")
-        val paragraph = textBox.addNewTextParagraph()
-        val run = paragraph.addNewTextRun()
-        val versetexts = arrayListOf<String>()
-        var curr = ""
-        try {
-            File("lyr/$songId.txt").readLines().forEach {
-                if (it.isBlank()) {
-                    versetexts.add(curr)
-                    curr = ""
-                    return@forEach
-                }
-                curr += it + "\n"
-            }
-        } catch (e: IOException) {
-            log.error("Failed to open lyrics file with id: $songId. ${e.localizedMessage}. Aborting")
-            e.printStackTrace()
-            exitProcess(2);
-        }
-        with(run) {
-            setText("$verse. ${versetexts[verse-1]}")
-            fontSize = 40.0
-            setFontColor(Color.WHITE)
-        }
-
-        if(index % 2 == 1 && index != verses.size - 1) {
-            log.debug("new verse slide, index=$index")
-            slide = ppt.createSlide()
-            slide.background.fillColor = Color.BLACK
-            textBox = slide.createTextBox()
-            textBox.anchor = Rectangle(HPAD, VPAD, SLIDE_W-2*HPAD, SLIDE_H-2*VPAD)
-        }
-    }
-}
-
-fun scriptureSlide(ppt: XMLSlideShow, path: String) {
-    val slide = ppt.createSlide()
-    slide.background.fillColor = Color.BLACK
-
-    slide.createTextBox().apply {
-        anchor = Rectangle(0, 0, SLIDE_W, SLIDE_H)
-    }.addNewTextParagraph().addNewTextRun().run {
-        setText(path)
-        setFontColor(Color.WHITE)
-        fontSize = 64.0
-    }
-}
-
-fun imageSlide(ppt: XMLSlideShow, image: BufferedImage) {
-    log.debug("Creating image slide ${image.width}x${image.height}, q=${image.height.toFloat() / image.width.toFloat()}")
-    assert(image.width == 800)
-    val pd = bufimgToPicData(ppt, image)
-
-    val slide = ppt.createSlide()
-    slide.background.fillColor = Color.BLACK
-    val chordPic = slide.createPicture(pd)
-
-
-    if(image.height < 700) { // single slide
-        log.debug("single slide")
-        chordPic.anchor = Rectangle(
-            (SLIDE_W - image.width * SLIDE_H / image.height) / 2, 0,
-            image.width * SLIDE_H / image.height, SLIDE_H
-        )
-    } else { // multi slide
-        log.debug("multiple slides")
-        chordPic.anchor = Rectangle((SLIDE_W - image.width) / 2, 0, image.width, image.height)
-        val overflowSlide = ppt.createSlide()
-        overflowSlide.background.fillColor = Color.BLACK
-        val p = overflowSlide.createPicture(pd)
-        p.anchor = Rectangle(
-            (SLIDE_W - image.width) / 2, SLIDE_H-image.height,
-            image.width, image.height
-        )
-        if(image.height > 2*SLIDE_H-10) {
-            log.info("Image doesn't fit on 2 slides")
-            arrayOf(slide, overflowSlide).forEach {
-                it.createTextBox().run {
-                    text = "BAD"
-                    fillColor = Color.RED
-                    anchor = Rectangle(0, 0, 75, 50)
-                }
-            }
-        }
-    }
 }
